a724536eb96a1fbe17e1f804e7019f4f2f9deedb
justMaxIt <maximfedor2015@gmail.com>
2020-12-07 17:18:04 +0200

    add: router, components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import CalendarContainer from "./components/Calendar/CalendarContainer";
+import ListContainer from "./components/List/ListContainer";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <CalendarContainer />
+        </Route>
+        <Route path="/list">
+          <ListContainer />
+        </Route>
+        <Route path="*">
+          <div>Path error: 404</div>
+        </Route>
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/components/List/ListContainer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ListContainer = () => {
+  return <div>List</div>;
+};
+
+export default ListContainer;
